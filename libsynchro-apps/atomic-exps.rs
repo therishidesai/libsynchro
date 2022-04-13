80a97c0a5ca9c87939708bb6fa0637530468d7e0
Rishi Desai <rdesai@anduril.com>
2022-04-13 04:55:11 +0000

    fix style

--- libsynchro-apps/atomic-exps.rs
+++ libsynchro-apps/atomic-exps.rs
@@ -7,7 +7,7 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::{thread, time};
 
 struct RCU {
-	data: AtomicUsize,
+    data: AtomicUsize,
     gen: AtomicUsize,
     rc: [AtomicUsize; 1024],
 }
@@ -25,30 +25,30 @@ fn main() {
     let arw = Arc::clone(&ar);
     let writer = thread::spawn(move || {
         for _ in 0..10 {
-			thread::sleep(time::Duration::from_millis(10));
-			let mut g = arw.gen.load(Ordering::Relaxed);
-			let mut d = arw.data.load(Ordering::Relaxed);
-			g +=1;
-			d +=1;
-			println!("gen {}, data {}", g, d);
-			arw.data.store(d, Ordering::Relaxed);
-			arw.gen.store(g, Ordering::Relaxed);
-		}
+            thread::sleep(time::Duration::from_millis(10));
+            let mut g = arw.gen.load(Ordering::Relaxed);
+            let mut d = arw.data.load(Ordering::Relaxed);
+            g +=1;
+            d +=1;
+            println!("gen {}, data {}", g, d);
+            arw.data.store(d, Ordering::Relaxed);
+            arw.gen.store(g, Ordering::Relaxed);
+        }
     });
     
     for i in 0..10 {
         let arr = Arc::clone(&ar);
         let handle = thread::spawn(move || {
-			for _ in 0..10 {
-				let mut rng = rand::thread_rng();
-				thread::sleep(time::Duration::from_millis(rng.gen_range(1..10)));
-				let num = arr.gen.load(Ordering::Relaxed);
-				let d = arr.data.load(Ordering::Relaxed);
-				let mut rc = arr.rc[num].load(Ordering::Relaxed);
-				rc += 1;
-				arr.rc[num].store(rc, Ordering::Relaxed);
-				println!("Reader {}: gen {}, data {}", i, num, d);
-			}
+            for _ in 0..10 {
+                let mut rng = rand::thread_rng();
+                thread::sleep(time::Duration::from_millis(rng.gen_range(1..10)));
+                let num = arr.gen.load(Ordering::Relaxed);
+                let d = arr.data.load(Ordering::Relaxed);
+                let mut rc = arr.rc[num].load(Ordering::Relaxed);
+                rc += 1;
+                arr.rc[num].store(rc, Ordering::Relaxed);
+                println!("Reader {}: gen {}, data {}", i, num, d);
+            }
         });
         handles.push(handle);
     }
@@ -57,7 +57,7 @@ fn main() {
         handle.join().unwrap();
     }
 
-	writer.join().unwrap();
+    writer.join().unwrap();
 
     println!("Final Gen: {}", ar.gen.load(Ordering::Relaxed));
     // for i in 0..ar.gen.load(Ordering::Relaxed)+1 {
