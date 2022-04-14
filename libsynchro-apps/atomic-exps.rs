60ec656f46ec4c0f161793a5dea0ec5489c794e5
Rishi Desai <rdesai@anduril.com>
2022-04-14 01:43:55 +0000

    fix style

--- libsynchro-apps/atomic-exps.rs
+++ libsynchro-apps/atomic-exps.rs
@@ -10,19 +10,19 @@ struct RCU<T> {
     data: AtomicPtr<T>,
     gen: AtomicUsize,
     rc: [AtomicIsize; 1024],
-	gen_data: [AtomicPtr<T>; 1024],
+    gen_data: [AtomicPtr<T>; 1024],
     done: AtomicBool,
 }
 
 fn main() {
     let rc_arr: [AtomicIsize; 1024] = arr![AtomicIsize::new(0); 1024];
-	let ptr = &mut 0;
-	let gen_data_arr: [AtomicPtr<isize>; 1024] = arr![AtomicPtr::new(ptr); 1024];
+    let ptr = &mut 0;
+    let gen_data_arr: [AtomicPtr<isize>; 1024] = arr![AtomicPtr::new(ptr); 1024];
     let r = RCU {
         data: AtomicPtr::new(Box::into_raw(Box::new(0))),
         gen: AtomicUsize::new(0),
         rc: rc_arr,
-		gen_data: gen_data_arr, 
+        gen_data: gen_data_arr, 
         done: AtomicBool::new(false),
     };
 
@@ -31,12 +31,12 @@ fn main() {
     let arc = Arc::clone(&ar);
     let cleanup = thread::spawn(move || {
         while (!arc.done.load(Ordering::SeqCst)){
-			let gens = arc.gen.load(Ordering::SeqCst);
+            let gens = arc.gen.load(Ordering::SeqCst);
             for i in 0..gens {
                 if (arc.rc[i].compare_exchange(0, -1, Ordering::SeqCst, Ordering::SeqCst) == Ok(0)) {
-					let ptr = arc.gen_data[i].load(Ordering::SeqCst);
-					println!("Going to Free gen {}, ptr: {:?}!", i, ptr);
-					//let b = unsafe { Box::from_raw(ptr) };
+                    let ptr = arc.gen_data[i].load(Ordering::SeqCst);
+                    println!("Going to Free gen {}, ptr: {:?}!", i, ptr);
+                    //let b = unsafe { Box::from_raw(ptr) };
                 }
             }
             thread::sleep(time::Duration::from_millis(10));
@@ -44,12 +44,12 @@ fn main() {
             // just an experiment
         }
 
-		println!("DONE!!!!");
-		let gens = arc.gen.load(Ordering::SeqCst);
+        println!("DONE!!!!");
+        let gens = arc.gen.load(Ordering::SeqCst);
         for i in 0..gens {
             if (arc.rc[i].compare_exchange(0, -1, Ordering::SeqCst, Ordering::SeqCst) == Ok(0)) {
                 println!("Going to Free gen {}!", i);
-				let ptr = arc.gen_data[i].load(Ordering::SeqCst);
+                let ptr = arc.gen_data[i].load(Ordering::SeqCst);
                 let b = unsafe { Box::from_raw(ptr) };
             }
         }
@@ -63,8 +63,8 @@ fn main() {
             let g = arw.gen.load(Ordering::SeqCst);
             let d = Box::into_raw(Box::new(g as isize));
             let old_d = arw.data.swap(d, Ordering::SeqCst);
-			let old_g = arw.gen.fetch_add(1, Ordering::SeqCst);
-			arw.gen_data[old_g].swap(old_d, Ordering::SeqCst);
+            let old_g = arw.gen.fetch_add(1, Ordering::SeqCst);
+            arw.gen_data[old_g].swap(old_d, Ordering::SeqCst);
             println!("gen {}, data {:?}", g, d);
         }
         arw.done.store(true, Ordering::SeqCst);
@@ -77,10 +77,10 @@ fn main() {
                 let mut rng = rand::thread_rng();
                 thread::sleep(time::Duration::from_millis(rng.gen_range(1..10)));
                 let num = arr.gen.load(Ordering::SeqCst);
-				arr.rc[num].fetch_add(1, Ordering::SeqCst);
+                arr.rc[num].fetch_add(1, Ordering::SeqCst);
                 let d = arr.data.load(Ordering::SeqCst);
                 println!("Reader {}: gen {}, data {:?}", i, num, d);
-				arr.rc[num].fetch_sub(1, Ordering::SeqCst);
+                arr.rc[num].fetch_sub(1, Ordering::SeqCst);
             }
         });
         handles.push(handle);
