a675c3f30da123202a6678fa0c208d023e19811e
Rishi Desai <desai.rishi1@gmail.com>
2022-04-16 20:17:04 -0700

    moved everything to an actual library, cleaned up example

--- libsynchro-apps/atomic-exps.rs
+++ libsynchro-apps/atomic-exps.rs
@@ -1,82 +1,15 @@
-use arr_macro::arr;
-
 use rand::Rng;
+use libsynchro::RCU;
 
 use std::sync::Arc;
-use std::sync::atomic::{AtomicUsize, AtomicIsize, AtomicPtr, AtomicBool, Ordering};
 use std::{thread, time};
-
-struct RCU<T> {
-    gen: AtomicUsize,
-    rc: [AtomicIsize; 1024],
-    gen_data: [AtomicPtr<T>; 1024],
-    done: AtomicBool,
-}
-
-impl<T> RCU<T> {
-	pub fn new(ptr: *mut T) -> Self {
-		let rc_arr: [AtomicIsize; 1024] = arr![AtomicIsize::new(0); 1024];
-		let gen_data_arr: [AtomicPtr<T>; 1024] = arr![AtomicPtr::new(ptr); 1024];
-		Self {
-			gen: AtomicUsize::new(0),
-			rc: rc_arr,
-			gen_data: gen_data_arr, 
-			done: AtomicBool::new(false),
-		}
-	}
-}
-
-fn rcu_init<T: 'static>(ar: &Arc<RCU<T>>) -> thread::JoinHandle<()> {
-	let arc = Arc::clone(ar);
-	thread::spawn(move || {
-        while !arc.done.load(Ordering::Relaxed) {
-            let gens = arc.gen.load(Ordering::Relaxed);
-            for i in 0..gens {
-                if arc.rc[i].compare_exchange(0, -1, Ordering::Release, Ordering::Relaxed) == Ok(0) {
-                    let ptr = arc.gen_data[i].load(Ordering::SeqCst);
-                    unsafe { Box::from_raw(ptr) };
-                    println!("Going to Free gen {}, ptr: {:?}!", i, ptr);
-                }
-            }
-            thread::sleep(time::Duration::from_millis(10));
-            // Spin loop I know...
-            // just an experiment
-        }
-
-        println!("DONE!!!!");
-        let gens = arc.gen.load(Ordering::Relaxed);
-        for i in 0..gens {
-            if arc.rc[i].compare_exchange(0, -1, Ordering::Release, Ordering::Relaxed) == Ok(0) {
-                println!("Going to Free gen {}!", i);
-                let ptr = arc.gen_data[i].load(Ordering::SeqCst);
-                unsafe { Box::from_raw(ptr) };
-            }
-        }
-    })
-}
-
-fn rcu_write_update<T: 'static>(arw: &Arc<RCU<T>>, newptr: *mut T) -> usize {
-	let curr_gen = arw.gen.load(Ordering::Relaxed);
-    arw.gen_data[curr_gen+1].store(newptr, Ordering::Relaxed);
-	arw.gen.fetch_add(1, Ordering::AcqRel)
-}
-
-fn rcu_read_lock<T: 'static>(arr: &Arc<RCU<T>>) -> usize {
-	let num = arr.gen.load(Ordering::Relaxed);
-    arr.rc[num].fetch_add(1, Ordering::Relaxed);
-	num
-}
-
-fn rcu_read_unlock<T: 'static>(arr: &Arc<RCU<T>>, gen: usize) {
-	arr.rc[gen].fetch_sub(1, Ordering::AcqRel);
-}
 
 fn main() {
 	let ptr = Box::into_raw(Box::new(0 as isize));
 	let r = RCU::new(ptr);
     let ar = Arc::new(r);
 
-    let cleanup = rcu_init(&ar);
+    let cleanup = libsynchro::rcu_init(&ar);
     
     let mut handles = vec![];
     let arw = Arc::clone(&ar);
@@ -84,10 +17,10 @@ fn main() {
         for i in 0..10 {
             thread::sleep(time::Duration::from_millis(10));
             let d = Box::into_raw(Box::new(i as isize));
-			let old_g = rcu_write_update(&arw, d);
+			let old_g = libsynchro::rcu_write_update(&arw, d);
             println!("gen {}, data {:?}", old_g+1, d);
         }
-        arw.done.store(true, Ordering::SeqCst);
+		libsynchro::synchronize_rcu(&arw);
     });
 
     // reader threads
@@ -97,10 +30,10 @@ fn main() {
             for _ in 0..10 {
                 let mut rng = rand::thread_rng();
                 thread::sleep(time::Duration::from_millis(rng.gen_range(1..10)));
-                let num = rcu_read_lock(&arr);
-                let d = arr.gen_data[num].load(Ordering::SeqCst);
-                println!("Reader {}: gen {}, data {:?}", i, num, d);
-				rcu_read_unlock(&arr, num);
+                let gen = libsynchro::rcu_read_lock(&arr);
+                let d = libsynchro::rcu_read_data(&arr, gen);
+                println!("Reader {}: gen {}, data {:?}", i, gen, d);
+				libsynchro::rcu_read_unlock(&arr, gen);
             }
         });
         handles.push(handle);
--- libsynchro/src/lib.rs
+++ libsynchro/src/lib.rs
@@ -1,18 +1,82 @@
-// libsynchro
-// rcu_init - start cleanup thread, and initialize handle
-// rcu_read_lock - bump refcount for the current generation, return handle for that generation
-// rcu_read_unlock - decrement the refcount for given generation
-// rcu_assign_pointer - create a new generation, assign new handle for that generation
-// call_rcu - assign a deleter for a given generation, defer the deletion for the cleanup thread in init
-// Note: call_rcu may be unnecessary, just use the Drop function, and writer only needs to do assign pointer
+use arr_macro::arr;
 
-use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::{thread, time};
+use std::sync::atomic::{AtomicPtr, AtomicBool, AtomicUsize, AtomicIsize, Ordering};
+
 
 const MAX_GENERATIONS: usize = 1024;
 
-pub struct RCU {
-	ptr: AtomicPtr,
-	gen: AtomicUsize,
-	rc:  [AtomicUsize; MAX_GENERATIONS],
+pub struct RCU<T> {
+    gen: AtomicUsize,
+    rc: [AtomicIsize; MAX_GENERATIONS],
+    gen_data: [AtomicPtr<T>; MAX_GENERATIONS],
+    done: AtomicBool,
+}
+
+impl<T> RCU<T> {
+	pub fn new(ptr: *mut T) -> Self {
+		let rc_arr: [AtomicIsize; MAX_GENERATIONS] = arr![AtomicIsize::new(0); 1024];
+		let gen_data_arr: [AtomicPtr<T>; MAX_GENERATIONS] = arr![AtomicPtr::new(ptr); 1024];
+		Self {
+			gen: AtomicUsize::new(0),
+			rc: rc_arr,
+			gen_data: gen_data_arr, 
+			done: AtomicBool::new(false),
+		}
+	}
+}
+
+pub fn rcu_init<T: 'static>(ar: &Arc<RCU<T>>) -> thread::JoinHandle<()> {
+	let arc = Arc::clone(ar);
+	thread::spawn(move || {
+        while !arc.done.load(Ordering::Relaxed) {
+            let gens = arc.gen.load(Ordering::Relaxed);
+            for i in 0..gens {
+                if arc.rc[i].compare_exchange(0, -1, Ordering::Release, Ordering::Relaxed) == Ok(0) {
+                    let ptr = arc.gen_data[i].load(Ordering::SeqCst);
+                    unsafe { Box::from_raw(ptr) };
+                    println!("Going to Free gen {}, ptr: {:?}!", i, ptr);
+                }
+            }
+			// Spin looping as an initial impl, can change this to something smarter
+            thread::sleep(time::Duration::from_millis(10));
+        }
+
+        println!("DONE!!!!");
+		// Synchronize RCU will set the done flag to true and we cleanup the rest
+        let gens = arc.gen.load(Ordering::Relaxed);
+        for i in 0..gens {
+            if arc.rc[i].compare_exchange(0, -1, Ordering::Release, Ordering::Relaxed) == Ok(0) {
+                println!("Going to Free gen {}!", i);
+                let ptr = arc.gen_data[i].load(Ordering::SeqCst);
+                unsafe { Box::from_raw(ptr) };
+            }
+        }
+    })
+}
+
+pub fn rcu_write_update<T: 'static>(arw: &Arc<RCU<T>>, newptr: *mut T) -> usize {
+	let curr_gen = arw.gen.load(Ordering::Relaxed);
+    arw.gen_data[curr_gen+1].store(newptr, Ordering::Relaxed);
+	arw.gen.fetch_add(1, Ordering::AcqRel)
+}
+
+pub fn rcu_read_lock<T: 'static>(arr: &Arc<RCU<T>>) -> usize {
+	let num = arr.gen.load(Ordering::Relaxed);
+    arr.rc[num].fetch_add(1, Ordering::Relaxed);
+	num
+}
+
+pub fn rcu_read_data<T: 'static>(arr: &Arc<RCU<T>>, gen: usize) -> *mut T {
+	arr.gen_data[gen].load(Ordering::SeqCst)
+}
+
+pub fn rcu_read_unlock<T: 'static>(arr: &Arc<RCU<T>>, gen: usize) {
+	arr.rc[gen].fetch_sub(1, Ordering::AcqRel);
+}
+
+pub fn synchronize_rcu<T: 'static>(ar: &Arc<RCU<T>>) {
+    ar.done.store(true, Ordering::SeqCst);
 }
 
