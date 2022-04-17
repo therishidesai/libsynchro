1292e455827d212dd2c5d8a662a72120af139094
Rishi Desai <desai.rishi1@gmail.com>
2022-04-16 20:17:27 -0700

    rustfmt

--- libsynchro-apps/atomic-exps.rs
+++ libsynchro-apps/atomic-exps.rs
@@ -1,26 +1,26 @@
-use rand::Rng;
 use libsynchro::RCU;
+use rand::Rng;
 
 use std::sync::Arc;
 use std::{thread, time};
 
 fn main() {
-	let ptr = Box::into_raw(Box::new(0 as isize));
-	let r = RCU::new(ptr);
+    let ptr = Box::into_raw(Box::new(0 as isize));
+    let r = RCU::new(ptr);
     let ar = Arc::new(r);
 
     let cleanup = libsynchro::rcu_init(&ar);
-    
+
     let mut handles = vec![];
     let arw = Arc::clone(&ar);
     let writer = thread::spawn(move || {
         for i in 0..10 {
             thread::sleep(time::Duration::from_millis(10));
             let d = Box::into_raw(Box::new(i as isize));
-			let old_g = libsynchro::rcu_write_update(&arw, d);
-            println!("gen {}, data {:?}", old_g+1, d);
+            let old_g = libsynchro::rcu_write_update(&arw, d);
+            println!("gen {}, data {:?}", old_g + 1, d);
         }
-		libsynchro::synchronize_rcu(&arw);
+        libsynchro::synchronize_rcu(&arw);
     });
 
     // reader threads
@@ -33,7 +33,7 @@ fn main() {
                 let gen = libsynchro::rcu_read_lock(&arr);
                 let d = libsynchro::rcu_read_data(&arr, gen);
                 println!("Reader {}: gen {}, data {:?}", i, gen, d);
-				libsynchro::rcu_read_unlock(&arr, gen);
+                libsynchro::rcu_read_unlock(&arr, gen);
             }
         });
         handles.push(handle);
